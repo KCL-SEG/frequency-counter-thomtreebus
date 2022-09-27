24f2077ec2591eecf43b4f86596ce00150f9b9ec
Thom Treebus <thomtreebus@gmail.com>
2022-09-27 13:16:20 +0100

    Add conversion to string for non-string items

--- frequencies.py
+++ frequencies.py
@@ -5,8 +5,8 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for item in items:
-        if item in frequencies:
-            frequencies[item] += 1
+        if str(item) in frequencies:
+            frequencies[str(item)] += 1
         else:
-            frequencies[item] = 1
+            frequencies[str(item)] = 1
     return frequencies
